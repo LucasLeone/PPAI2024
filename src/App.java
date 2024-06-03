40f866aec3c9fdcc8aa390c774641f7862c78225
lucasleone03 <lucasagustinleone2003@gmail.com>
2024-06-03 12:46:43 -0300

    finalizado entrega 1

--- src/App.java
+++ src/App.java
@@ -3,7 +3,6 @@ public class App {
     public static DB db = new DB();
 
     public static void main(String[] args) throws Exception {
-        // CASO DE USO ANTERIOR, nos brinda estos datos: :)
         GestorImportarActualizaciones gestor = new GestorImportarActualizaciones(db);
         gestor.habilitarPantalla();
     }
